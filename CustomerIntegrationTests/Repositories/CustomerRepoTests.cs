450472b31633eb20cb5de60b42827d6d80ef4692
Buse Duran <buseduran500@gmail.com>
2024-08-18 21:03:15 +0300

    test: add repotest

---
+++ CustomerIntegrationTests/Repositories/CustomerRepoTests.cs
@@ -0,0 +1,24 @@
+﻿using Application.Mapping;
+using AutoMapper;
+
+namespace CustomerIntegrationTests.Repositories
+{
+    public class CustomerRepoTests : IClassFixture<SharedDatabaseFixture>
+    {
+        private readonly IMapper _mapper;
+        private SharedDatabaseFixture Fixture { get; }
+
+        public CustomerRepoTests( IMapper mapper, SharedDatabaseFixture fixture )
+        {
+            Fixture = fixture;
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new CustomerProfile());
+            });
+
+            _mapper = configuration.CreateMapper();
+        }
+        // Tests go here
+    }
+}
